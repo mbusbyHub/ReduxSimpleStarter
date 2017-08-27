7c769d484aec187c9c81e66d816089f6f24e4c2e
Michael Busby <michaelb@clearlyinnovative.com>
2017-08-27 16:47:42 -0400

    Add youtube api key and search component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+import YOUTUBE_API_KEY from './api_keys';
+
+const ytApiKey = YOUTUBE_API_KEY;
 
 // Create a new component. This component should
 // produce some html
-const App = function() {
-  return <div>Hi!</div>;
+const App = () => {
+  return( 
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it 
 // on the page (in the DOM)
-React.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));
