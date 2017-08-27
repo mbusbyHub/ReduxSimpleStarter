2820ef43bf15a207dd702d3908b1fe258f584da2
Michael Busby <michaelb@clearlyinnovative.com>
2017-08-27 19:37:22 -0400

    Create video list and item templates

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 import { YOUTUBE_API_KEY } from './api_keys';
 
-YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, data => {
-  console.log(data);
-});
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
 
-// Create a new component. This component should
-// produce some html
-const App = () => {
-  return( 
-    <div>
-      <SearchBar />
-    </div>
-  );
+    YTSearch({key: YOUTUBE_API_KEY, term: 'star wars'}, videos => {
+      // this.setState({ videos: videos });
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return( 
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
-// Take this component's generated HTML and put it 
-// on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
